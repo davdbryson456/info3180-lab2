109e7a7c4a59618c81510bf5e6744939277f4678
David Bryson <david.bryson@mymona.uwi.edu>
2019-02-10 00:39:00 +0000

    Lab2

--- app/views.py
+++ app/views.py
@@ -7,7 +7,7 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
+import datetime 
 
 ###
 # Routing for your application.
@@ -23,7 +23,11 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
-
+    
+@app.route('/profile/')
+def profile():
+    
+    return render_template('profile.html',name="Tallulah")
 
 ###
 # The functions below should be applicable to all Flask apps.
@@ -52,6 +56,11 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+    
+#def format_date_joined():
+#    now = datetime.datetime.now() 
+#    date_joined = datetime.date(2019, 2, 7) 
+#    print "Joined " + date_joined.strftime("%B, %Y")
 
 
 if __name__ == '__main__':
